bb2da0e372b55d8a5e80f1b0151f5c8422b6092c
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-06-01 18:04:32 +0700

    changed table sctructure and added antd

--- src/components/Person/PersonList.jsx
+++ src/components/Person/PersonList.jsx
@@ -4,35 +4,33 @@ import './PersonList.css'
 const PersonList = props => {
 
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Имя</th>
-          <th>Фамилия</th>
-        </tr>
-        {
-          props.persons.map(person => {
-            return (
-              <tr>
-                <td key={person.id}>
-                  {
-                    person.firstName
-                  }
-                </td>
-                <td key={person.id}>
-                  {
-                    person.lastName
-                  }
-                </td>
-                <div>
-                  <button>1</button>
-                  <button>2</button>
-                </div>
-              </tr>
-            )
-          })
-        }
-      </table>
+    <div className='table'>
+      <div className='table__title'>
+        <div className='table__item'>Имя</div>
+        <div className='table__item'>Фамилия</div>
+      </div>
+      {
+        props.persons.map(person => {
+          return (
+            <div className='table__row' key={person.id}>
+              <div className='table__item'>
+                {
+                  person.firstName
+                }
+              </div>
+              <div className='table__item'>
+                {
+                  person.lastName
+                }
+              </div>
+              <div>
+                <button>1</button>
+                <button>2</button>
+              </div>
+            </div>
+          )
+        })
+      }
       <button>Добавить сотрудника</button>
     </div>
   );
